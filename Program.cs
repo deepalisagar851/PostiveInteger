7049b4e31fbd79373c745ebe94346b915186d64f
Deepali <96642348+deepalisagar851@users.noreply.github.com>
2022-04-08 14:13:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PostiveInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a Integer:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (x > 0)
+            {
+                if (x % 3 == 0)
+                {
+                    Console.WriteLine("The integer is a postive number and multiple of 3");
+                }
+                else if (x % 7 == 0)
+                {
+
+                    Console.WriteLine("The integer is a postive number and multiple of 7");
+                }
+            }
+            else { 
+ 
+                    Console.WriteLine("The integer is not a postive number ");
+            }
+        }
+    }
+}
